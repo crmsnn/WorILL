99ed0f02fec54d5e98f83e136491303a111e7aef
Crimson <cjeth03@gmail.com>
2024-07-12 20:33:00 -0600

    vibeos wrokin

--- src/level.js
+++ src/level.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './level.css';
 
-function Level({place, title, creator, link = "https://www.youtube.com/watch?v=LtlyeDAJR7A", fps, id})  {
+function Level({place, title, creator, link, fps, id})  {
     return(
         <div className="level">
                 <h1><em>{place}</em></h1>
-                <iframe title= 'showcase' src={link} width = '352px' height='198px' />
+                <iframe width="352" height="198"
+                    src={link}>
+                </iframe>
                 <h2><em>{title}</em></h2>
                 <h3>{creator}</h3>
                 <br/>
